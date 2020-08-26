a95bf91fc53503a63de1f9ade366a52770d277a2
t-k-satoh <syakuyanaoto@gmail.com>
2020-08-26 19:48:15 +0900

    test: 💍 mod api test

--- src/api/categories/list/api.test.ts
+++ src/api/categories/list/api.test.ts
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 import status from 'http-status';
 import request from 'supertest';
-import { app } from '../../../app/app';
 import { mockApp } from '../../test-utils';
 import { Result } from '../types';
 import { PATH } from './constants';
@@ -10,51 +9,17 @@ import { list } from '.';
 dotenv.config();
 
 describe(__filename, () => {
-    describe('authorized', () => {
-        test('HTTP status code', async () => {
-            const response = await request(mockApp.use('/', list)).get(PATH);
+    test('疎通確認', async () => {
+        const response = await request(mockApp.use('/', list)).get(PATH);
+        const body: Result = response.body;
+        const header = response.header;
 
-            expect(response.status).toBe(status.OK);
-        });
+        expect(response.status).toBe(status.OK);
 
-        test('response payload', async () => {
-            const response = await request(mockApp.use('/', list)).get(PATH);
-            const body: Result = response.body;
+        expect('categories' in body).toBe(true);
+        expect('page' in body).toBe(true);
 
-            expect('categories' in body).toBe(true);
-            expect('page' in body).toBe(true);
-        });
-
-        test('response headers', async () => {
-            const response = await request(mockApp.use('/', list)).get(PATH);
-            const header = response.header;
-
-            expect(header['content-type']).toBe('application/json; charset=utf-8');
-            expect(header['x-powered-by']).toBe('Express');
-        });
-    });
-    describe('unauthorized', () => {
-        test('HTTP status code', async () => {
-            const response = await request(app.use('/', list)).get(PATH);
-
-            expect(response.status).toBe(status.UNAUTHORIZED);
-        });
-
-        test('response payload', async () => {
-            const response = await request(app.use('/', list)).get(PATH);
-            const body: Result = response.body;
-            const text = response.text;
-
-            expect(body).toEqual({});
-            expect(text).toBe(status[401]);
-        });
-
-        test('response headers', async () => {
-            const response = await request(app.use('/', list)).get(PATH);
-            const header = response.header;
-
-            expect(header['content-type']).toBe('text/html; charset=utf-8');
-            expect(header['x-powered-by']).toBe('Express');
-        });
+        expect(header['content-type']).toBe('application/json; charset=utf-8');
+        expect(header['x-powered-by']).toBe('Express');
     });
 });
--- src/api/categories/retrieve/api.test.ts
+++ src/api/categories/retrieve/api.test.ts
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 import status from 'http-status';
 import request from 'supertest';
-import { app } from '../../../app/app';
 import { PREFIX } from '../../constants';
 import { mockApp } from '../../test-utils';
 import { list } from '../list';
@@ -12,86 +11,32 @@ import { retrieve } from '.';
 dotenv.config();
 
 describe(__filename, () => {
-    describe('authorized', () => {
-        test('HTTP status code', async () => {
-            const responseList = await request(mockApp.use('/', list)).get(LIST_PATH);
-            const responseListBody: Result = responseList.body;
-            const id = responseListBody.categories[0].category_id;
+    test('疎通確認', async () => {
+        const responseList = await request(mockApp.use('/', list)).get(LIST_PATH);
+        const responseListBody: Result = responseList.body;
+        const id = responseListBody.categories[0].category_id;
 
-            const response = await request(mockApp.use('/', retrieve)).get(`${PREFIX}/categories/${id}`);
+        const response = await request(mockApp.use('/', retrieve)).get(`${PREFIX}/categories/${id}`);
 
-            expect(response.status).toBe(status.OK);
-        });
+        expect(response.status).toBe(status.OK);
 
-        test('response payload', async () => {
-            const responseList = await request(mockApp.use('/', list)).get(LIST_PATH);
-            const responseListBody: Result = responseList.body;
-            const id = responseListBody.categories[0].category_id;
+        const body: Result['categories'][number] = response.body;
 
-            const response = await request(mockApp.use('/', retrieve)).get(`${PREFIX}/categories/${id}`);
-            const body: Result['categories'][number] = response.body;
+        expect('category_id' in body).toBe(true);
+        expect('name' in body).toBe(true);
+        expect('nick_name' in body).toBe(true);
+        expect('created_at' in body).toBe(true);
+        expect('updated_at' in body).toBe(true);
 
-            expect('category_id' in body).toBe(true);
-            expect('name' in body).toBe(true);
-            expect('nick_name' in body).toBe(true);
-            expect('created_at' in body).toBe(true);
-            expect('updated_at' in body).toBe(true);
+        expect(typeof body.category_id).toBe('string');
+        expect(typeof body.name).toBe('string');
+        expect(typeof body.nick_name).toBe('string');
+        expect(typeof body.created_at).toBe('string');
+        expect(typeof body.updated_at).toBe('string');
 
-            expect(typeof body.category_id).toBe('string');
-            expect(typeof body.name).toBe('string');
-            expect(typeof body.nick_name).toBe('string');
-            expect(typeof body.created_at).toBe('string');
-            expect(typeof body.updated_at).toBe('string');
-        });
+        const header = response.header;
 
-        test('response headers', async () => {
-            const responseList = await request(mockApp.use('/', list)).get(LIST_PATH);
-            const responseListBody: Result = responseList.body;
-            const id = responseListBody.categories[0].category_id;
-
-            const response = await request(mockApp.use('/', retrieve)).get(`${PREFIX}/categories/${id}`);
-            const header = response.header;
-
-            expect(header['content-type']).toBe('application/json; charset=utf-8');
-            expect(header['x-powered-by']).toBe('Express');
-        });
-    });
-    describe('unauthorized', () => {
-        test('HTTP status code', async () => {
-            const responseList = await request(mockApp.use('/', list)).get(LIST_PATH);
-            const responseListBody: Result = responseList.body;
-            const id = responseListBody.categories[0].category_id;
-
-            const response = await request(app.use('/', retrieve)).get(`${PREFIX}/categories/${id}`);
-
-            expect(response.status).toBe(status.UNAUTHORIZED);
-        });
-
-        test('response payload', async () => {
-            const responseList = await request(mockApp.use('/', list)).get(LIST_PATH);
-            const responseListBody: Result = responseList.body;
-            const id = responseListBody.categories[0].category_id;
-
-            const response = await request(app.use('/', retrieve)).get(`${PREFIX}/categories/${id}`);
-
-            const body: Result = response.body;
-            const text = response.text;
-
-            expect(body).toEqual({});
-            expect(text).toBe(status[401]);
-        });
-
-        test('response headers', async () => {
-            const responseList = await request(mockApp.use('/', list)).get(LIST_PATH);
-            const responseListBody: Result = responseList.body;
-            const id = responseListBody.categories[0].category_id;
-
-            const response = await request(app.use('/', retrieve)).get(`${PREFIX}/categories/${id}`);
-
-            const header = response.header;
-
-            expect(header['content-type']).toBe('text/html; charset=utf-8');
-            expect(header['x-powered-by']).toBe('Express');
-        });
+        expect(header['content-type']).toBe('application/json; charset=utf-8');
+        expect(header['x-powered-by']).toBe('Express');
     });
 });
