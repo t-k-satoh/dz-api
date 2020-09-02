d090a442c5adf745c7a4e1c943a42e372887f9bd
t-k-satoh <syakuyanaoto@gmail.com>
2020-09-02 20:54:44 +0900

    test: 💍 app test

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,28 +1,10 @@
 import status from 'http-status';
-import { routers } from '../api';
+// import { routers } from '../api';
 import { app } from './app';
 
 export const newApp = app;
 
-routers.forEach((router) => {
-    newApp.use('/', router);
-});
-
 newApp.use('/', (_req, res) => {
     console.log('ないよ！');
     res.status(status.NOT_FOUND).send(status[404]);
-});
-
-newApp.get('/products', function (req, res) {
-    console.log(req.params);
-    res.send(req.params);
-});
-
-newApp.use((_req, _res, next) => {
-    const err = new Error('Not Found');
-
-    next({
-        ...err,
-        status: 404,
-    });
 });
