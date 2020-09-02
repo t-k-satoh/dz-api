c1015cf754908cff8c73cc956a9dc4142d4c49a4
t-k-satoh <syakuyanaoto@gmail.com>
2020-09-02 20:57:39 +0900

    feat: 🎸 app

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,10 +1,22 @@
 import status from 'http-status';
-// import { routers } from '../api';
+import { routers } from '../api';
 import { app } from './app';
 
 export const newApp = app;
 
+routers.forEach((router) => {
+    newApp.use('/', router);
+});
+
 newApp.use('/', (_req, res) => {
-    console.log('ないよ！');
     res.status(status.NOT_FOUND).send(status[404]);
+});
+
+newApp.use((_req, _res, next) => {
+    const err = new Error('Not Found');
+
+    next({
+        ...err,
+        status: 404,
+    });
 });
